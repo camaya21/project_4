5b5882608eb638a6b683c7515a052c4a76628af3
camaya21 <cindymamaya21@gmail.com>
2023-01-30 15:30:58 -0500

    Created Home & About page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
+from django.views import View
+from django.http import HttpResponse
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
