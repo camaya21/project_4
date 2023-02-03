5b23d839b19ecc3e004457fd1844ac8eeaa269cf
camaya21 <cindymamaya21@gmail.com>
2023-02-03 10:16:12 -0500

    Installing dotenv

--- project_4/settings.py
+++ project_4/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv, find_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import AutoConfig
+load_dotenv(find_dotenv())
 
-config = AutoConfig(search_path='/opt/envs/project_4')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
